9a11852fe22e7cf47c16ce5ec916124b34996e12
Veronur <luccadelchiaro@gmail.com>
2019-11-25 08:45:41 -0300

    Update server.py

--- server.py
+++ server.py
@@ -10,14 +10,14 @@ def Tarefas():
     r=requests.get('http://{}:5000/Tarefa/'.format(ip))
     if flask.request.method == 'POST':
         nome = flask.request.form['data']
-        requests.post('http://{}:5000/Tarefa/'.format(ip), data={'data':r.text})        
-    return {'nome' : nome}          
+        requests.post('http://{}:5000/Tarefa/'.format(ip), data={'data':nome})        
+    return {'Nomes': r.text}          
     
 
 @app.route('/Tarefa/<idT>', methods=["DELETE"])
 def Delete(idT):
-    requests.delete('http://{0}:5000/Tarefa/{1}'.format(ip,idT))
-    return ""
+        requests.delete('http://{0}:5000/Tarefa/{1}'.format(ip,idT))
+        return ""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
